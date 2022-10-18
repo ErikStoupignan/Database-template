8769798c97a5440a96b1eea6b2b2be76dbb7fafd
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-10-18 15:49:28 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 
+/* Project 1 */
 CREATE TABLE animals(
 id            INT GENERATED ALWAYS AS IDENTITY,
 name    VARCHAR(250),
@@ -9,3 +10,6 @@ neutered     boolean,
 weight_kg decimal,
 PRIMARY KEY(id)
 );
+
+/* Project 2 */
+ALTER TABLE animals ADD species VARCHAR(250);
