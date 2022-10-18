a6991917da84c4c29aa8d0751f8fc3bba3d0bed3
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-10-18 17:35:18 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -38,3 +38,10 @@ ROLLBACK TO firstmove;
 UPDATE animals SET weight_kg = weight_kg*-1 WHERE weight_kg<0;
 COMMIT;
 
+/* Queries */
+SELECT COUNT( name ) FROM animals;
+SELECT COUNT( * ) FROM animals WHERE escape_attempts = 0;
+SELECT AVG( weight_kg ) FROM animals;
+SELECT neutered, AVG( escape_attempts ) FROM animals GROUP BY neutered ORDER BY AVG DESC LIMIT 1;
+SELECT species, MIN( weight_kg ), MAX( weight_kg ) FROM animals GROUP BY species;
+SELECT species, AVG( escape_attempts ) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
