7438e1fdb6d9b19dddf841716e5e7ec1877fe155
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-10-19 18:28:12 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -31,11 +31,8 @@ PRIMARY KEY(id)
 ALTER TABLE animals ADD owners_id VARCHAR(250);
 ALTER TABLE animals ADD species_id VARCHAR(250);
 
-ALTER TABLE species
-ADD CONSTRAINT fk_species
-FOREIGN KEY(id)
-REFERENCES animals(id)
-ON DELETE CASCADE;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE;
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY (owners_id) REFERENCES owners (id) ON DELETE CASCADE;
 
 UPDATE animals SET species_id='2' WHERE name LIKE '%mon';
 UPDATE animals SET species_id='1' WHERE name NOt LIKE '%mon';
